eb56abf8fe7e8cfb95168ff0effec590b0d73b04
IRSHAD KHAN PATTAN <khan70907090@gmail.com>
2026-01-05 21:10:45 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 // ⚠️ SECURITY NOTE: In a real production app, use environment variables.
 // For the hackathon, hardcoding this here fixes your "Invalid API Key" error instantly.
 const firebaseConfig = {
-  apiKey: "AIzaSyAqXWwX1c3yI6-gcLwY0V5gTMa4w8400io",
-  authDomain: "financial-report-generat-62321.firebaseapp.com",
-  projectId: "financial-report-generat-62321",
-  storageBucket: "financial-report-generat-62321.firebasestorage.app",
-  messagingSenderId:  "420829512424",
-  appId: "1:420829512424:web:818ddccb29124d54c93f5d",
-  measurementId: "G-15322KDWKD"
+  apiKey: "---",
+  authDomain: "---",
+  projectId: "---",
+  storageBucket: "---",
+  messagingSenderId:  "---",
+  appId: "---",
+  measurementId: "---"
 };
 
 // Initialize Firebase
